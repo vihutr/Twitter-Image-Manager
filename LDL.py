36644fd4441131333e35653561a499e4e564d95d
vihutr <vihutr@gmail.com>
2022-09-02 08:44:43 -0700

    renaming variables

--- LDL.py
+++ LDL.py
@@ -6,8 +6,7 @@ from decouple import config
 # To set your enviornment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
 bearer_token = config('bearer_token')
-
-testingid = config('test_user_id')
+testuid = config('test_user_id')
 
 
 def create_url():
@@ -18,14 +17,16 @@ def create_url():
     # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
     # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
     # source, text, and withheld
-    tweet_fields = "tweet.fields=author_id"
-    media_fields = "media.fields=url"
+    tweet_fields = "tweet.fields=attachments,text"
+    media_fields = "media.fields=preview_image_url,type,url"
     # Be sure to replace your-user-id with your own user ID or one of an authenticating user
     # You can find a user ID by using the user lookup endpoint
-    id = ""
+    id = testuid
     # You can adjust ids to include a single Tweets.
     # Or you can add to up to 100 comma-separated IDs
-    url = "https://api.twitter.com/2/users/{}/liked_tweets".format(id)
+    #url = "https://api.twitter.com/2/users/{}/liked_tweets".format(id)
+    url = "https://api.twitter.com/2/users/{}/retweeted_by".format(id)
+    urlmedia = "https://api.twitter.com/2/tweets?ids={}&expansions=attachments.media_keys&media.fields=duration_ms,height,media_key,preview_image_url,public_metrics,type,url,width,alt_text'".format(tid)
     return url, media_fields
 
 def bearer_oauth(r):
@@ -38,9 +39,9 @@ def bearer_oauth(r):
     return r
 
 
-def connect_to_endpoint(url, media_fields):
+def connect_to_endpoint(url, tweet_fields):
     response = requests.request(
-        "GET", url, auth=bearer_oauth, params=media_fields)
+        "GET", url, auth=bearer_oauth, params=tweet_fields)
     print(response.status_code)
     if response.status_code != 200:
         raise Exception(
@@ -52,8 +53,8 @@ def connect_to_endpoint(url, media_fields):
 
 
 def main():
-    url, media_fields = create_url()
-    json_response = connect_to_endpoint(url, media_fields)
+    url, tweet_fields = create_url()
+    json_response = connect_to_endpoint(url, tweet_fields)
     print(json.dumps(json_response, indent=4, sort_keys=True))
 
 
