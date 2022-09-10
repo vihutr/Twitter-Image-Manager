ee9fef47fdfb3e4ee462c56981723808609a087d
vihutr <vihutr@gmail.com>
2022-09-09 18:30:57 -0700

    Cleaned up code, added overloaded method + proper fields

--- LDL.py
+++ LDL.py
@@ -3,45 +3,39 @@ import os
 import json
 
 from decouple import config
-# To set your enviornment variables in your terminal run the following line:
-# export 'BEARER_TOKEN'='<your_bearer_token>'
+
+# Using virtual environment tokens
 bearer_token = config('bearer_token')
 testuid = config('test_user_id')
 
+# To set your environment variables in terminal run the following:
+# export 'BEARER_TOKEN'='<your_bearer_token>'
 
-def create_url():
-    # Tweet fields are adjustable.
-    # Options include:
-    # attachments, author_id, context_annotations,
-    # conversation_id, created_at, entities, geo, id,
-    # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
-    # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
-    # source, text, and withheld
+def create_url(id):
     tweet_fields = "tweet.fields=attachments,text"
-    media_fields = "media.fields=preview_image_url,type,url"
-    # Be sure to replace your-user-id with your own user ID or one of an authenticating user
-    # You can find a user ID by using the user lookup endpoint
-    id = testuid
+    media_fields = "&expansions=attachments.media_keys&media.fields=preview_image_url,type,url"
     # You can adjust ids to include a single Tweets.
     # Or you can add to up to 100 comma-separated IDs
-    #url = "https://api.twitter.com/2/users/{}/liked_tweets".format(id)
-    url = "https://api.twitter.com/2/users/{}/retweeted_by".format(id)
-    urlmedia = "https://api.twitter.com/2/tweets?ids={}&expansions=attachments.media_keys&media.fields=duration_ms,height,media_key,preview_image_url,public_metrics,type,url,width,alt_text'".format(tid)
-    return url, media_fields
+    url = "https://api.twitter.com/2/users/{}/liked_tweets".format(id)
+    #url = "https://api.twitter.com/2/users/{}/retweeted_by".format(id)
+    return url, tweet_fields+media_fields
+
+def create_url(id, mode):
+    tweet_fields = "tweet.fields=attachments,text"
+    media_fields = "&expansions=attachments.media_keys&media.fields=preview_image_url,type,url"
+    url = "https://api.twitter.com/2/users/{}/liked_tweets".format(id)
+    #url = "https://api.twitter.com/2/users/{}/retweeted_by".format(id)
+    return url, tweet_fields+media_fields
 
 def bearer_oauth(r):
-    """
-    Method required by bearer token authentication.
-    """
-
     r.headers["Authorization"] = f"Bearer {bearer_token}"
     r.headers["User-Agent"] = "v2LikedTweetsPython"
     return r
 
-
 def connect_to_endpoint(url, tweet_fields):
     response = requests.request(
         "GET", url, auth=bearer_oauth, params=tweet_fields)
+    print(response.url)
     print(response.status_code)
     if response.status_code != 200:
         raise Exception(
@@ -53,7 +47,8 @@ def connect_to_endpoint(url, tweet_fields):
 
 
 def main():
-    url, tweet_fields = create_url()
+    #inp = input('STATEMENT')
+    url, tweet_fields = create_url(testuid)
     json_response = connect_to_endpoint(url, tweet_fields)
     print(json.dumps(json_response, indent=4, sort_keys=True))
 
