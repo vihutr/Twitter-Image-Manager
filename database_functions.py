cad2c2755fc3a832c545d850cfcdee92e0d1b382
vihutr <vihutr@gmail.com>
2022-10-04 19:45:56 -0700

    reordered functions to be more sensible

--- database_functions.py
+++ database_functions.py
@@ -11,6 +11,18 @@ def init_db():
     cur.close()
     conn.close()
 
+def add_to_db(filename, localpath, t_id, media_key, m_type, t_url, i_url):
+    conn = sqlite3.connect("database.db")
+    cur = conn.cursor()
+    insert_query = '''INSERT INTO Images
+        (filename, localpath, id, media_key, type, tweet_url, image_url) 
+        VALUES (?, ?, ?, ?, ?, ?, ?);'''
+    data_tuple = (filename, localpath, t_id, media_key, m_type, t_url, i_url)
+    cur.execute(insert_query, data_tuple)
+    conn.commit()
+    cur.close()
+    conn.close()
+
 def update_db(filename, localpath, media_key, m_type, i_url):
     conn = sqlite3.connect("database.db")
     cur = conn.cursor()
@@ -22,18 +34,6 @@ def update_db(filename, localpath, media_key, m_type, i_url):
     WHERE media_key = ?'''
     data_tuple = (filename, localpath, m_type, i_url, media_key)
     cur.execute(update_query, data_tuple)
-    conn.commit()
-    cur.close()
-    conn.close()
-
-def add_to_db(filename, localpath, t_id, media_key, m_type, t_url, i_url):
-    conn = sqlite3.connect("database.db")
-    cur = conn.cursor()
-    insert_query = '''INSERT INTO Images
-        (filename, localpath, id, media_key, type, tweet_url, image_url) 
-        VALUES (?, ?, ?, ?, ?, ?, ?);'''
-    data_tuple = (filename, localpath, t_id, media_key, m_type, t_url, i_url)
-    cur.execute(insert_query, data_tuple)
     conn.commit()
     cur.close()
     conn.close()
